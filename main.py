0d6847d00b25f24be0a82972b4bd617826861cd2
NightReflection <132771382+Night-Reflection@users.noreply.github.com>
2025-03-28 07:35:36 +0000

    test

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "<p>Home</p>"
+    return render_template("home.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+app.run(debug=True)
